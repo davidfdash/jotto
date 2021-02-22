7928bd61b01a1aa1a1a90640d7c56b19ccf43a01
Dave <davestoneyfuller@gmail.com>
2021-02-21 20:16:23 -0600

    revamped python

--- jotto.py
+++ jotto.py
@@ -1,18 +1,18 @@
-from collections import Counter
+import re
 
 word1 = input("Player 1, please enter a 5 letter word: ")
 guess = ""
 #guess a word
-
+#is it the word? if so, you win, if not proceed
 while guess != word1:
     guess = input("Guess a five letter word: ")
-    dict1 = Counter(word1)
-    dict2 = Counter(guess)
-    commonDict = dict1 & dict2
-    commonCharacters = list(commonDict.elements())
-    
-#is it the word? if so, you win, if not proceed
-
+    c = 0
+    for i in word1: 
+        if re.search(i,guess): 
+            c=c+1
+    print(c)
+else:
+    print("you win!")
 #compare letters to word1 and return number of letters in common
 
 #repeat until it is the word
