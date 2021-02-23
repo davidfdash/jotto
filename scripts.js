2eac127cc01377795cce026bfe3751fc7c6a193c
Dave <davestoneyfuller@gmail.com>
2021-02-22 21:46:44 -0600

    added some click logic bases

--- scripts.js
+++ scripts.js
@@ -1,3 +1,18 @@
 var str = new String("Demo Text");
          document.write(str.strike());
          alert(str.strike());
+
+
+<div id="container">
+    <span>0</span><span>0</span><span>0</span><span>0</span> <span>0</span><span>0</span><span>0</span><span>0</span> <span>0</span><span>0</span><span>0</span><span>0</span>
+</div>
+
+var container = document.getElementById("container");
+if (container.addEventListener) {
+    container.addEventListener('click', clickHandler, false);
+}
+else if (container.attachEvent) {
+    container.attachEvent('onclick', function(e) {
+        return clickHandler.call(container, e || window.event);
+    });
+}
